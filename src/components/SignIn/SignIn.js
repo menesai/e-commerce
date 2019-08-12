f4159cbe4d3cce4775d64bf2d78e3544caea753c
Uriel Meneses <saidmeneses1@live.com>
2019-08-12 13:02:17 -0500

    deploying app to use fb, twitter, auth login

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,13 +3,31 @@ import './SignIn.scss'
 import {Form, Header, Button, Icon} from 'semantic-ui-react'
 import {Link} from 'react-router-dom';
 import firebase from '../../firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    // signInSuccessUrl: '/signin',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        // Avoid redirects after sign-in.
+        signInSuccessWithAuthResult: () => false
+      }
+  };
 
 class SignIn extends Component {
     constructor(){
         super();
         this.state={
             email: '',
-            password: ''
+            password: '',
+            logged: false
         }
     }
 
@@ -23,52 +41,71 @@ class SignIn extends Component {
         this.setState({[e.target.name]: e.target.value})
     }
 
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user =>{
+            this.setState({logged:!!user})
+        })
+    }
+
+
     render() {
-        // console.log(this.state)
+        console.log(firebase.auth().currentUser)
         const {email, password}=this.state
-        return (
-            <div className='signIn'>
-
-                <Form size='small' className='SignInForm' onSubmit={this.handleForm} style={{maxWidth: 450}}>
-                   <Header as='h2' color='blue' textAlign='center'>
-                    <Icon name='shopping bag' color='black' size='small'/>
-                        Sign in with your email
-                   </Header>
-
-                    <Form.Input
-                    icon='user'
-                    iconPosition='left'
-                    name='email'
-                    type='email'
-                    placeholder='enter email'
-                    value={email}
-                    required
-                    onChange={this.handleChange}
-                    />
-                     {/* <label>Email</label> */}
-                    <Form.Input
-                    icon='lock'
-                    iconPosition='left'
-                    name='password'
-                    type='password'
-                    placeholder='enter password'
-                    value={password}
-                    required
-                    onChange={this.handleChange}
-                    />
-
-                     <Button type='submit' value='Submit' color='blue'>Login</Button>
-                        <div className='register-message'>
-                            <label className='register-link'>Don't have an Account? <Link to='#'> click here</Link></label>
-                        </div>
-                </Form>
-
-                <div className='provider'>
-                    google, facebook, github, login
+        if(!this.state.logged){
+            return (
+                <div className='signIn'>
+    
+                    <Form size='small' className='SignInForm' onSubmit={this.handleForm} style={{maxWidth: 450}}>
+                       <Header as='h2' color='blue' textAlign='center'>
+                        <Icon name='shopping bag' color='black' size='small'/>
+                            Sign in with your email
+                       </Header>
+    
+                        <Form.Input
+                        icon='user'
+                        iconPosition='left'
+                        name='email'
+                        type='email'
+                        placeholder='enter email'
+                        value={email}
+                        required
+                        onChange={this.handleChange}
+                        />
+                         {/* <label>Email</label> */}
+                        <Form.Input
+                        icon='lock'
+                        iconPosition='left'
+                        name='password'
+                        type='password'
+                        placeholder='enter password'
+                        value={password}
+                        required
+                        onChange={this.handleChange}
+                        />
+    
+                         <Button type='submit' value='Submit' color='blue'>Login</Button>
+                            <div className='register-message'>
+                                <label className='register-link'>Don't have an Account? <Link to='#'> click here</Link></label>
+                            </div>
+                    </Form>
+    
+                    <div className='provider'>
+                        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}>
+    
+                        </StyledFirebaseAuth>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+
+        } else{
+            return(
+                <div>
+                    your signed in
+                    <button onClick={() => firebase.auth().signOut()}>log out</button>
+                </div>
+            )
+        }
     }
 }
 
-export default SignIn
+export default SignIn;
